0c54b5e8b94571c49ec2d31604f44e417ee5a353
Moussahif Saida <93975470+saidamoussahif@users.noreply.github.com>
2022-11-03 11:42:10 +0100

    Delete todo.model.js

--- DB/models/todo.model.js
+++
@@ -1,8 +0,0 @@
-const {Schema, model} = require("mongoose") 
-
-const todoSchema = new Schema({
-    Title: {type: String},
-    Description: {type: String},
-})
-
-module.export = model("todo", todoSchema)
