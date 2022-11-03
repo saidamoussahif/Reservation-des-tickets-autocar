567f8547c03720cc4aef9458dc0bee9ede15ed7a
Moussahif Saida <93975470+saidamoussahif@users.noreply.github.com>
2022-11-03 11:42:44 +0100

    Delete index.js

--- config/index.js
+++
@@ -1,5 +0,0 @@
-module.exports ={
-    PORT: process.env.PORT || 5500,
-    DB_URI: process.env.DB_URI || "mongodb://127.0.0.1:27017/BookingSystemDB",
-    MONGOOSE_OPTIONS:  {useNewUrlParser: true, useUnifiedTopology: true }
-} 
